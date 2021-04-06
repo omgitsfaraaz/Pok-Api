2206c74765598a55aff6d0837cbef44fc0ff6d2a
unknown <mohsinsait23@gmail.com>
2021-04-06 19:04:07 +0530

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Navbar() {
+    return (
+        <div className="nav-container">
+            <nav className="navbar">
+                <h1 id="nav-logo">PokéApi</h1>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
